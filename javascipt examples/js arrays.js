6f0ca88becd4a000d876288388dc30d1b596cc29
Cavid <57284252+cavidk@users.noreply.github.com>
2021-04-03 21:54:21 +0400

    Update js arrays.js
    comments have removed

--- javascipt examples/js arrays.js
+++ javascipt examples/js arrays.js
@@ -1,7 +1,7 @@
 // Arraylar uzerinde iw/ array e eleman nasil eklemeli!
- //var array = ["Java","Python","C++","C#"];
+ var array = ["Java","Python","C++","C#"];
         // array[4] = "php";
          
 //array push() metodu;
-//    array.push("Go");
-//    alert(array.length); //length shows quantity of elements inside array function!
+    array.push("Go");
+    alert(array.length); //length shows quantity of elements inside array function!
